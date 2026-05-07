377cf6221f59a9a048a091b43cafbff43e5824fa
Mingg <nhatminh16704@gmail.com>
2026-05-07 18:04:25 +0700

    feat: add cache invalidation pipeline behavior

---
+++ src/Application/Common/Interfaces/ICacheInvalidator.cs
@@ -0,0 +1,7 @@
+﻿namespace Heven.Api.Application.Common.Interfaces;
+
+public interface ICacheInvalidator
+{
+    // Trả về danh sách các Key cần phải xóa khỏi Redis / MemoryCache
+    string[] CacheKeys { get; }
+}
